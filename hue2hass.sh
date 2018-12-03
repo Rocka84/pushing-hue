11975c60e8cce67bf8be6592f30b1034b7ef6627
Rocka84 <f.dillmeier@gmail.com>
2018-12-04 00:14:37 +0100

    reindented code

--- hue2hass.sh
+++ hue2hass.sh
@@ -25,7 +25,7 @@ hass_token=""
 #  ash /path/to/hue2hasd.sh > /dev/null &
 
 ## Logs
-# By default logs are wtitten to stdout and the file 'hass.log'
+# By default logs are written to stdout and the file 'hass.log'
 # in the same folder as the script
 # The file is truncated to 100 entries when reaching 200 entries.
 
@@ -48,11 +48,11 @@ log() {
   echo "$(logdate) " "$@" | tee -a $log_file
   logs=$(( logs + 1))
   if [ $logs -gt 200 ]; then
-  	cp "$log_file" "$log_file".old
-  	tail -99 "$log_file" > "$log_file".old 
-  	mv "$log_file".old "$log_file"
-  	logs=99
-  	log "logs truncated"
+      cp "$log_file" "$log_file".old
+      tail -99 "$log_file" > "$log_file".old
+      mv "$log_file".old "$log_file"
+      logs=99
+      log "logs truncated"
   fi
 }
 
@@ -71,50 +71,50 @@ queryHueSensor() {
 }
 
 getProp() {
-	echo "$hue_status" | sed '/^"'"$1"'":/!d;s/^"'"$1"'":"\?//;s/"\?,$//'
+    echo "$hue_status" | sed '/^"'"$1"'":/!d;s/^"'"$1"'":"\?//;s/"\?,$//'
 }
 
 hueLightToHass() {
-	queryHueLight "$1"
+    queryHueLight "$1"
 
-	id="$(getProp uniqueid)"
-	name="$(getProp name)"
-	bri="$(getProp bri)"
-	on="$(getProp on)"
+    id="$(getProp uniqueid)"
+    name="$(getProp name)"
+    bri="$(getProp bri)"
+    on="$(getProp on)"
 
-	# [ "$bri" == "1" ] && return
+    # [ "$bri" == "1" ] && return
 
-	[ "$on" == "true" ] && on="on" || on="off"
-	entity_id="light.$(getEntityId "$name")"
-	state="$(prepareLightState "$entity_id" "$on" "$bri")"
+    [ "$on" == "true" ] && on="on" || on="off"
+    entity_id="light.$(getEntityId "$name")"
+    state="$(prepareLightState "$entity_id" "$on" "$bri")"
 
-	pushToHass "$entity_id" "$state"
+    pushToHass "$entity_id" "$state"
 }
 
 hueSensorToHass() {
-	queryHueSensor "$1"
+    queryHueSensor "$1"
 
-	name="$(getProp "name")"
-	entity_id="sensor.$(getEntityId "$name")"
+    name="$(getProp "name")"
+    entity_id="sensor.$(getEntityId "$name")"
 
-	type="$(getProp "type")"
-	case "$type" in
-	  "ZLLSwitch") attribute="buttonevent";;
-	  "ZLLPresence") attribute="presence";;
-	  "ZLLTemperature") attribute="temperature";;
-	  *) info "unsupported type '$type' for '$name'"; return;;
-	esac
+    type="$(getProp "type")"
+    case "$type" in
+        "ZLLSwitch") attribute="buttonevent";;
+        "ZLLPresence") attribute="presence";;
+        "ZLLTemperature") attribute="temperature";;
+        *) info "unsupported type '$type' for '$name'"; return;;
+    esac
 
-	state="$(getProp "$attribute")"
-	# id="$(getProp "uniqueid")"
+    state="$(getProp "$attribute")"
+    # id="$(getProp "uniqueid")"
 
-	[ -z "$state" ] && return
-	[ "$state" == "true" ] && state="on"
-	[ "$state" == "false" ] && state="off"
-	[ "$attribute" == "temperature" ] && state="$(echo "$state" | sed 's/\([0-9]\{2\}\)$/.\1/')"
-	state="$(prepareState "$entity_id" "$state")"
+    [ -z "$state" ] && return
+    [ "$state" == "true" ] && state="on"
+    [ "$state" == "false" ] && state="off"
+    [ "$attribute" == "temperature" ] && state="$(echo "$state" | sed 's/\([0-9]\{2\}\)$/.\1/')"
+    state="$(prepareState "$entity_id" "$state")"
 
-	pushToHass "$entity_id" "$state"
+    pushToHass "$entity_id" "$state"
 }
 
 getEntityId() {
@@ -133,11 +133,11 @@ prepareLightState() {
 }
 
 prepareState() {
-	# info prepareLightState
-	update="$(date -u +"%Y-%m-%dT%H:%M:%S.000000+00:00")"
+    # info prepareLightState
+    update="$(date -u +"%Y-%m-%dT%H:%M:%S.000000+00:00")"
     getHassState "$1" \
-      | sed 's/"state": *"[^"]*"/"state":"'$2'"/' \
-	  | sed 's/"last_\(changed\|updated\)":[^,]*,/"last_\1":"'"$update"'",/g'
+        | sed 's/"state": *"[^"]*"/"state":"'$2'"/' \
+        | sed 's/"last_\(changed\|updated\)":[^,]*,/"last_\1":"'"$update"'",/g'
 }
 
 getHassState() {
@@ -146,14 +146,14 @@ getHassState() {
 }
 
 parseLogEntry() {
-	index="$(echo "$1"| sed 's/^.*, ID:\([^,]*\),.*$/\1/')"
-	if echo "$1" | grep -q 'T:CLIP.*R:0\|T:RULES.*R:2'; then
-	  info "Light $index changed!"
-	  hueLightToHass "$index"
-	elif echo "$1" | grep -q 'T:ZIGBEE.*R:6'; then
-	  info "Sensor $index changed!"
-	  hueSensorToHass "$index"
-	fi
+    index="$(echo "$1"| sed 's/^.*, ID:\([^,]*\),.*$/\1/')"
+    if echo "$1" | grep -q 'T:CLIP.*R:0\|T:RULES.*R:2'; then
+        info "Light $index changed!"
+        hueLightToHass "$index"
+    elif echo "$1" | grep -q 'T:ZIGBEE.*R:6'; then
+        info "Sensor $index changed!"
+        hueSensorToHass "$index"
+    fi
 }
 
 info "verbose logging enabled"
@@ -161,6 +161,6 @@ log "reading hue logs"
 
 logread -e 'T:CLIP.*R:0\|T:RULES.*R:2\|T:ZIGBEE.*R:6' -f \
   | while read log_entry; do
-	parseLogEntry "$log_entry"
+    parseLogEntry "$log_entry"
 done
 
